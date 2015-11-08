ab8e2c65b38029e7693c90d25ee8411f4047b922
qza <kokovic.zoran@gmail.com>
2015-11-08 09:44:30 +0100

    simple example runner - updated message typo

--- src/main/scala/simple/MainRunnerWithMessage.scala
+++ src/main/scala/simple/MainRunnerWithMessage.scala
@@ -9,7 +9,7 @@ object MainRunnerWithMessage {
 
     printf("""|Welcome to scala!
              |This raw string can contain anything
-             |It starts and ends with tripple quotes
+             |It starts and ends with triple quotes
              |New lines are marked with pipe character
              |Call to the stripMargin for nice printing""".stripMargin);
 
